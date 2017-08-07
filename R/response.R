090b4e1c83f1d895e43bbf8520d8f2eb9f33cc10
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-08-07 15:53:27 +0200

    Don't reformat file body

--- R/response.R
+++ R/response.R
@@ -407,6 +407,8 @@ Response <- R6Class('Response',
         format_body = function() {
             if (is.raw(private$BODY)) {
                 private$BODY
+            } else if (is.scalar(private$BODY) && names(private$BODY) == 'file') {
+                private$BODY
             } else {
                 paste(as.character(private$BODY), collapse = '\n')
             }
